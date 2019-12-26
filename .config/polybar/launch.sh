2b11ab3ecef1a304129aaaedb1e80a8a52e7df5b
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2019-12-26 11:14:44 +0100

    Automatic setting of polybar monitor based on autorandr.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,6 +6,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Determine monitor
+if [[ $(autorandr | awk '/(current)/ {print $1}') == "work" ]]; then
+	CURRENT_POLYBAR_MONITOR=DP2
+#elif [[ $(autorandr | awk '/(current)/ {print $1}') == "home" ]]; then
+#	CURRENT_POLYBAR_MONITOR=HDMI2
+fi
+
 # Launch top bar
-echo "---" | tee -a /tmp/top.log
-polybar top >>/tmp/top.log 2>&1 &
+echo "---" | tee -a /tmp/polybar_top.log
+POLYBAR_MONITOR=$CURRENT_POLYBAR_MONITOR polybar top >>/tmp/polybar_top.log 2>&1 &
