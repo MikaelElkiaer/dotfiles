f549be759f3a1eabb2b09d46ecb6c9582397081d
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2019-12-27 22:11:37 +0100

    VPN icon and fuzzy monitor matching.

--- .config/polybar/scripts/vpn-openvpn-isrunning.sh
+++ .config/polybar/scripts/vpn-openvpn-isrunning.sh
@@ -3,7 +3,7 @@
 connection=$(pgrep -a openvpn$ | head -n 1 | awk '{print $NF }' | cut -d '.' -f 1)
 
 if [ -n "$connection" ]; then
-    echo "VPN: connection"
+    echo " $connection"
 else 
     echo ""
 fi
