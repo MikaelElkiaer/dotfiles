72742592f724461da127041b61e872d0cb3fa7ab
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2019-12-30 14:56:59 +0100

    Switched to UXrvt as terminal emulator.

--- create-links.sh
+++ create-links.sh
@@ -7,6 +7,7 @@ ln -sfn $(pwd)/.p10k.zsh ~/.p10k.zsh
 ln -sfn $(pwd)/.vimrc ~/.vimrc
 ln -sfn $(pwd)/.xinitrc ~/.xinitrc
 ln -sfn $(pwd)/.xprofile ~/.xprofile
+ln -sfn $(pwd)/.Xresources ~/.Xresources
 ln -sfn $(pwd)/.zprofile ~/.zprofile
 ln -sfn $(pwd)/.zshrc ~/.zshrc
 ln -sfn $(pwd)/bin ~/bin
