7d00b93a0eb7df1c4c237dc3b9ad1ceac3fb032b
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2020-01-09 22:23:29 +0100

    Added secondary polybar(s) - updated config and launch script.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,13 +6,16 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Determine monitor
-if [[ $(autorandr | awk '/(current)/ {print $1}') == "work" ]]; then
-	CURRENT_POLYBAR_MONITOR=DP-2
-#elif [[ $(autorandr | awk '/(current)/ {print $1}') == "home" ]]; then
-#	CURRENT_POLYBAR_MONITOR=HDMI-2
-fi
+# Determine monitors
+PRIMARY_MONITOR=$(xrandr | awk '$2 == "connected" && $3 == "primary" { print $1 }')
+SECONDARY_MONITORS=$(xrandr | awk '$2 == "connected" && $3 != "primary" { print $1 }')
 
-# Launch top bar
-echo "---" | tee -a /tmp/polybar_top.log
-POLYBAR_MONITOR=$CURRENT_POLYBAR_MONITOR polybar top >>/tmp/polybar_top.log 2>&1 &
+# Launch bars
+echo "---" | tee -a /tmp/polybar_primary.log
+POLYBAR_MONITOR=$PRIMARY_MONITOR polybar primary >>/tmp/polybar_primary.log 2>&1 &
+
+for m in $SECONDARY_MONITORS
+do
+	echo "---" | tee -a /tmp/polybar_secondary_$m.log
+	POLYBAR_MONITOR=$m polybar secondary >>/tmp/polybar_secondary_$m.log 2>&1 &
+done
