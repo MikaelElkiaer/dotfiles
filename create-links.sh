11a38121b567b314f09b1ef7b296aa5e67c8fd62
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2020-02-08 13:05:09 +0100

    Updated create-links script.

--- create-links.sh
+++ create-links.sh
@@ -4,10 +4,10 @@ ln -sfn $(pwd)/.config ~/.config
 ln -sfn $(pwd)/.gitconfig ~/.gitconfig
 ln -sfn $(pwd)/.gtkrc-2.0 ~/.gtkrc-2.0
 ln -sfn $(pwd)/.p10k.zsh ~/.p10k.zsh
+ln -sfn $(pwd)/.profile ~/.profile
+ln -sfn $(pwd)/.themes ~/.themes
 ln -sfn $(pwd)/.vimrc ~/.vimrc
 ln -sfn $(pwd)/.xinitrc ~/.xinitrc
-ln -sfn $(pwd)/.xprofile ~/.xprofile
 ln -sfn $(pwd)/.Xresources ~/.Xresources
-ln -sfn $(pwd)/.zprofile ~/.zprofile
 ln -sfn $(pwd)/.zshrc ~/.zshrc
 ln -sfn $(pwd)/bin ~/bin
