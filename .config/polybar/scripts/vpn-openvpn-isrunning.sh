aa9990dd85d5c6f7f45cee8b43f69b50d6b87f3c
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2020-02-09 13:05:24 +0100

    Removed unused openvpn polybar script.

--- .config/polybar/scripts/vpn-openvpn-isrunning.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-connection=$(pgrep -a openvpn$ | head -n 1 | awk '{print $NF }' | cut -d '.' -f 1)
-
-if [ -n "$connection" ]; then
-    echo " %{F#fff}$connection"
-else 
-    echo ""
-fi
