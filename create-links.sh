0e80260fa0744b9bb17ee6813f73134704fd6d80
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2020-12-14 12:35:16 +0100

    Removed stuff not needed for WSL.

--- create-links.sh
+++ create-links.sh
@@ -2,12 +2,7 @@
 
 ln -sfn $(pwd)/.config ~/.config
 ln -sfn $(pwd)/.gitconfig ~/.gitconfig
-ln -sfn $(pwd)/.gtkrc-2.0 ~/.gtkrc-2.0
 ln -sfn $(pwd)/.p10k.zsh ~/.p10k.zsh
-ln -sfn $(pwd)/.profile ~/.profile
-ln -sfn $(pwd)/.themes ~/.themes
 ln -sfn $(pwd)/.vimrc ~/.vimrc
-ln -sfn $(pwd)/.xinitrc ~/.xinitrc
-ln -sfn $(pwd)/.Xresources ~/.Xresources
 ln -sfn $(pwd)/.zshrc ~/.zshrc
-ln -sfn $(pwd)/bin ~/bin
+
