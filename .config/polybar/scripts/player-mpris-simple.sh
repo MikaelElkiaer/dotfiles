0c19c12e685e85b21b42c59ee421798567c1f781
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2021-05-17 09:07:55 +0200

    Added weather to polybar.

--- .config/polybar/scripts/player-mpris-simple.sh
+++ .config/polybar/scripts/player-mpris-simple.sh
@@ -3,9 +3,9 @@
 player_status=$(playerctl -p spotifyd status 2> /dev/null)
 
 if [ "$player_status" = "Playing" ]; then
-    echo "ﱘ %{F#fff}$(playerctl -p spotifyd metadata artist) - $(playerctl -p spotifyd metadata title)"
+    echo "ﱘ $(playerctl -p spotifyd metadata artist) - $(playerctl -p spotifyd metadata title)"
 elif [ "$player_status" = "Paused" ]; then
-    echo " %{F#fff}$(playerctl -p spotifyd metadata artist) - $(playerctl -p spotifyd metadata title)"
+    echo " $(playerctl -p spotifyd metadata artist) - $(playerctl -p spotifyd metadata title)"
 else
     echo ""
 fi
