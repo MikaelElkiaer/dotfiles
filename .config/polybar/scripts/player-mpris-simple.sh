e5e095bd3459c6e994d62961c56fb5124109d545
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2021-11-03 10:45:00 +0100

    Generalized playerctl commands.

--- .config/polybar/scripts/player-mpris-simple.sh
+++ .config/polybar/scripts/player-mpris-simple.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-player_status=$(playerctl -p spotifyd status 2> /dev/null)
+player_status=$(playerctl status 2> /dev/null)
 
 if [ "$player_status" = "Playing" ]; then
-    echo "ﱘ $(playerctl -p spotifyd metadata artist) - $(playerctl -p spotifyd metadata title)"
+    echo "ﱘ $(playerctl metadata artist) - $(playerctl metadata title)"
 elif [ "$player_status" = "Paused" ]; then
-    echo " $(playerctl -p spotifyd metadata artist) - $(playerctl -p spotifyd metadata title)"
+    echo " $(playerctl metadata artist) - $(playerctl metadata title)"
 else
     echo ""
 fi
