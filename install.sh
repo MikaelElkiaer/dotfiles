e074e049f919621a928bf067b8582730fb18eba6
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2022-04-18 14:23:25 +0200

    Updated install script to include linking and no-confirm.

--- install.sh
+++ install.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
+# backup and create symlinks for all config files
+for f in $(find . -type f -name ".*"); do
+  [ -s ~/$f && mv ~/$f ~/$f.bak ];
+  ln -sf $PWD/$f ~/$f;
+done
+
+# backup and create symlinks for everything in .config
+for f in $(ls .config); do
+  [ -s ~/.config/$f ] && mv ~/.config/$f ~/.config/$f.bak;
+  ln -sfn $PWD/.config/$f ~/.config/$f;
+done
+
 # install pacmanfile for synchronizing packages
-yay -S pacmanfile
+yay -S --noconfirm pacmanfile
 
 # restore packages
-pacmanfile sync
+pacmanfile --noconfirm sync
 
 # change to zsh
 chsh -s /usr/bin/zsh
