18e5e5b454e3373bdd0b17be495574e06529976f
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2022-04-18 21:50:13 +0200

    Got lvim config up-to-date with external dependencies.

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -10,7 +10,7 @@ an executable
 
 -- general
 lvim.log.level = "warn"
-lvim.format_on_save = true
+lvim.format_on_save = false
 lvim.colorscheme = "dracula"
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
@@ -144,6 +144,60 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- Additional Plugins
 lvim.plugins = {
   { "Mofiqul/dracula.nvim" },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    module = "nvim-treesitter-textobjects",
+    after = "nvim-treesitter",
+    config = function()
+      require("custom.plugins.treesitter-textobjects").setup()
+    end,
+  },
+  {
+    "phaazon/hop.nvim",
+    as = "hop",
+    config = function()
+      require 'hop'.setup()
+    end,
+  },
+  {
+    "chaoren/vim-wordmotion",
+  },
+  {
+    "Pocco81/AutoSave.nvim",
+    config = function()
+      require 'autosave'.setup {
+        conditions = {
+          filename_is_not = { "config.lua" }
+        }
+      }
+    end
+  },
+  { "wellle/targets.vim" },
+  {
+    "Hoffs/omnisharp-extended-lsp.nvim",
+    requires = { { "nvim-telescope/telescope.nvim" } },
+  },
+  {
+    "vim-test/vim-test"
+  },
+  {
+    "francoiscabrol/ranger.vim",
+    requires = { { "rbgrouleff/bclose.vim" } }
+  },
+  {
+    "nvim-telescope/telescope-dap.nvim",
+    requires = { { "mfussenegger/nvim-dap" }, { "nvim-telescope/telescope.nvim" } },
+    config = function()
+      require('telescope').load_extension('dap')
+    end
+  },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    requires = { { "mfussenegger/nvim-dap", "nvim-treesitter/nvim-treesitter" } },
+    config = function()
+      require("nvim-dap-virtual-text").setup()
+    end
+  }
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
@@ -158,8 +212,9 @@ vim.g.wordmotion_prefix = "<BS>"
 vim.g["test#csharp#runner"] = "dotnettest"
 vim.g["test#strategy"] = "neovim"
 vim.g["test#neovim#term_position"] = "below 15"
---vim.g.ranger_map_keys = 0
+vim.g.ranger_map_keys = 0
 
+-- lualine
 if lvim.builtin.lualine.active then
   require('lualine').setup {
     options = {
@@ -167,3 +222,124 @@ if lvim.builtin.lualine.active then
     }
   }
 end
+
+-- nvim-treesitter-textobjects
+require 'nvim-treesitter.configs'.setup {
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+      },
+      goto_previous_end = {
+        ["[M"] = "@function.outer",
+      },
+    },
+  },
+}
+
+-- nvim-dap
+local dap = require("dap")
+dap.adapters.netcoredbg = {
+  type = 'executable',
+  command = '/usr/bin/netcoredbg',
+  args = { '--interpreter=vscode' }
+}
+dap.configurations.cs = {
+  {
+    type = "netcoredbg",
+    name = "launch - netcoredbg",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to dll', vim.fn.getcwd(), 'file')
+    end,
+  },
+  {
+    type = "netcoredbg",
+    name = "attach - netcoredbg",
+    request = "attach",
+    processId = require('dap.utils').pick_process,
+  },
+}
+
+-- toggleterm
+-- local Terminal = require('toggleterm.terminal').Terminal
+-- local default_terminal = Terminal:new({
+--   direction = "horizontal",
+--   hidden = true
+-- })
+
+-- function DefaultTerminalToggle()
+--   default_terminal:toggle()
+-- end
+
+-- lspconfig
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "efm", "omnisharp" })
+
+local lspconfig = require "lspconfig"
+local pid = vim.fn.getpid()
+local on_attach = require("lvim.lsp").common_on_attach
+local capabilities = require("lvim.lsp").common_capabilities()
+
+lspconfig.omnisharp.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "cs" },
+  handlers = {
+    ["textDocument/definition"] = require('omnisharp_extended').handler,
+  },
+  cmd = { "/usr/bin/omnisharp", "--languageserver", "--hostPID", tostring(pid) },
+  root_dir = function(fname)
+    return vim.fn.getcwd()
+  end,
+}
+
+lspconfig.efm.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "markdown" },
+  init_options = { documentFormatting = false },
+  settings = {
+    rootMarkers = { ".git/" },
+    languages = {
+      markdown = {
+        {
+          lintCommand = "markdownlint -s",
+          lintStdin = true,
+          lintFormats = { "%f:%l %m", "%f:%l:%c %m", "%f: %l: %m" }
+        }
+      }
+    }
+  }
+}
+
+local opts = { noremap = true, silent = true }
+local map = vim.api.nvim_set_keymap
+map("n", "gw", "<cmd>HopWord<cr>", opts)
+map("n", "gl", "<cmd>HopLine<cr>", opts)
+map("n", "gf", "<cmd>HopChar1<cr>", opts)
+map("n", "g/", "<cmd>HopPattern<cr>", opts)
+lvim.builtin.which_key.mappings["t"] = {
+  name = "Test",
+  n = { "<cmd>TestNearest<CR>", "Nearest" },
+  f = { "<cmd>TestFile<CR>", "File" },
+  s = { "<cmd>TestSuite<CR>", "Suite" },
+  l = { "TestLast<CR>", "Last" },
+  v = { "<cmd>TestVisit<CR>-\\><C-n><C-w>l", "Visit" }
+}
+lvim.builtin.which_key.mappings["r"] = { "<cmd>RangerCurrentFile<CR>", "Ranger file" }
