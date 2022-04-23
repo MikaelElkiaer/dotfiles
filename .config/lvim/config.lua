63a3670721c9791e4453ab372c8dcca6aed755d6
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2022-04-23 20:55:37 +0200

    Added plugins for lua debugging.

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -206,6 +206,10 @@ lvim.plugins = {
     config = function()
       require("telescope").load_extension("ui-select")
     end
+  },
+  {
+    "jbyuki/one-small-step-for-vimkind",
+    requires = { { "mfussenegger/nvim-dap" } },
   }
 }
 
@@ -285,6 +289,29 @@ dap.configurations.cs = {
     processId = require('dap.utils').pick_process,
   },
 }
+
+dap.configurations.lua = {
+  {
+    type = 'nlua',
+    request = 'attach',
+    name = "Attach to running Neovim instance",
+    host = function()
+      local value = vim.fn.input('Host [127.0.0.1]: ')
+      if value ~= "" then
+        return value
+      end
+      return '127.0.0.1'
+    end,
+    port = function()
+      local val = tonumber(vim.fn.input('Port: '))
+      assert(val, "Please provide a port number")
+      return val
+    end,
+  }
+}
+dap.adapters.nlua = function(callback, config)
+  callback({ type = 'server', host = config.host, port = config.port })
+end
 
 -- toggleterm
 -- local Terminal = require('toggleterm.terminal').Terminal
