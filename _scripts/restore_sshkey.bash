5fb1a4f07a85c28eb3fffcb6bcaf6db9e779357c
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2022-05-22 19:38:23 +0200

    Added chmod 400 after restoring ssh keys.

--- _scripts/restore_sshkey.bash
+++ _scripts/restore_sshkey.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 bw list items --search "ssh - laptop - arch" | jq -r '.[0].fields[] | select(.name=="public") | .value' > ~/.ssh/id_ed25519.pub
+chmod 400 ~/.ssh/id_ed25519.pub
 
 bw list items --search "ssh - laptop - arch" | jq -r '.[0].fields[] | select(.name=="private") | .value' > ~/.ssh/id_ed25519
+chmod 400 ~/.ssh/id_ed25519
