8dfe57bed0c79e1ee2fea79a0375a3b990fa2f7c
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2022-06-03 14:50:15 +0200

    Remove 2.1 from dotnet install.

--- _scripts/install_dotnet.bash
+++ _scripts/install_dotnet.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 wget https://dot.net/v1/dotnet-install.sh && chmod +x dotnet-install.sh
-sudo ./dotnet-install.sh --channel 2.1 --version latest --install-dir /opt/dotnet
 sudo ./dotnet-install.sh --channel 3.1 --version latest --install-dir /opt/dotnet
 sudo ./dotnet-install.sh --channel 6.0 --version latest --install-dir /opt/dotnet
