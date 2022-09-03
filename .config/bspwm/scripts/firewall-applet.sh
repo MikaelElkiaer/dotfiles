f49b6234ad0f8f3e110a366881b4ab72218a58ca
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2022-09-03 20:21:40 +0200

    Trying to fix firewall-applet startup script

--- .config/bspwm/scripts/firewall-applet.sh
+++ .config/bspwm/scripts/firewall-applet.sh
@@ -1,1 +1,9 @@
-pgrep firewall-applet &> /dev/null || firewall-applet
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q firewall-applet
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x firewall-applet >/dev/null; do sleep 1; done
+
+firewall-applet &
