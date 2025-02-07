171f5ee998a74a0b9380b0595124c3238146706d
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2025-02-07 14:26:49 +0100

    doc(hm): Copy stateVersion comment from docs

--- home/nixos/.config/home-manager/home.nix
+++ home/nixos/.config/home-manager/home.nix
@@ -25,7 +25,15 @@ in
   home.username = username;
   home.homeDirectory = homeDirectory;
 
-  home.stateVersion = "24.05"; # WARN: Do not change
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "24.05";
 
   home.packages = with pkgs; [
     amazon-ecr-credential-helper
