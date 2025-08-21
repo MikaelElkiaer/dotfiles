95049706fd63101c232701ad293e94f61283cd4b
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2025-08-21 15:37:57 +0200

    nix-darwin: Configure sketchybar

--- etc/nix-darwin/flake.nix
+++ etc/nix-darwin/flake.nix
@@ -30,7 +30,11 @@
           # - e.g. `/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"`
           homebrew = {
             enable = true;
+            brews = [
+            ];
             casks = [
+              "font-hack-nerd-font" # Needed for sketchybar
+              "font-sketchybar-app-font" # Needed for sketchybar
               "kitty"
               "podman-desktop"
               "trex"
@@ -85,8 +89,8 @@
             #               tall monitor (anything higher than wide) gets vertical orientation
             default-root-container-orientation = "auto";
 
-            on-focused-monitor-changed = [];
-            on-focus-changed = ["move-mouse window-lazy-center"];
+            on-focused-monitor-changed = [ ];
+            on-focus-changed = [ "move-mouse window-lazy-center" ];
 
             # Possible values: (qwerty|dvorak)
             # See https://nikitabobko.github.io/AeroSpace/guide#key-mapping
@@ -278,14 +282,149 @@
                 "mode main"
               ];
             };
+
+            # Notify Sketchybar about workspace change
+            exec-on-workspace-change = [
+              "/bin/bash"
+              "-c"
+              "sketchybar --trigger aerospace_workspace_change FOCUSED_WORKSPACE=$AEROSPACE_FOCUSED_WORKSPACE PREV_WORKSPACE=$AEROSPACE_PREV_WORKSPACE"
+            ];
           };
 
           services.sketchybar.enable = true;
           services.sketchybar.config = ''
-            sketchybar --bar height=25 display=main
+            # A simple sketchybar config for aerospace users to get started with
+            # Not too different from the base starting config!
+
+            PLUGIN_DIR="~/.config/sketchybar/plugins"
+
+            ##### Bar Appearance #####
+            # Configuring the general appearance of the bar.
+            # These are only some of the options available. For all options see:
+            # https://felixkratz.github.io/SketchyBar/config/bar
+            # If you are looking for other colors, see the color picker:
+            # https://felixkratz.github.io/SketchyBar/config/tricks#color-picker
+
+            sketchybar --bar position=top height=25 blur_radius=20 color=0x40000000
+
+            ##### Changing Defaults #####
+            # We now change some default values, which are applied to all further items.
+            # For a full list of all available item properties see:
+            # https://felixkratz.github.io/SketchyBar/config/items
+
+            default=(
+              padding_left=5
+              padding_right=5
+              icon.font="Hack Nerd Font:Bold:17.0"
+              label.font="Hack Nerd Font:Semibold:17.0"
+              icon.color=0xffffffff
+              label.color=0xffffffff
+              icon.padding_left=4
+              icon.padding_right=4
+              label.padding_left=4
+              label.padding_right=4
+              updates=on
+            )
+            sketchybar --default "''\${default[@]}"
+
+            ##### Adding aeropsace layouts #####
+            # Add the aerospace events we specified in aerospace.toml
+            sketchybar --add event aerospace_workspace_change
+            sketchybar --add event aerospace_monitor_change
+
+            for sid in $(aerospace list-workspaces --all); do
+              sketchybar --add item space.$sid left \
+                --subscribe space.$sid aerospace_workspace_change \
+                --set space.$sid \
+                display="$(
+                  v=$(aerospace list-windows --workspace "$sid" --format "%{monitor-appkit-nsscreen-screens-id}" | cut -c1)
+                  echo "''\${v:-1}"
+                )" \
+                drawing=off \
+                background.color=0x44ffffff \
+                background.corner_radius=5 \
+                background.drawing=on \
+                background.border_color=0xAAFFFFFF \
+                background.border_width=0 \
+                background.height=25 \
+                icon="$sid" \
+                icon.padding_left=10 \
+                icon.shadow.distance=4 \
+                icon.shadow.color=0xA0000000 \
+                label.font="sketchybar-app-font:Regular:16.0" \
+                label.padding_right=20 \
+                label.padding_left=0 \
+                label.y_offset=-1 \
+                label.shadow.drawing=off \
+                label.shadow.color=0xA0000000 \
+                label.shadow.distance=4 \
+                click_script="aerospace workspace $sid" \
+                script="$CONFIG_DIR/plugins/aerospace.sh $sid"
+            done
+
+            # Load Icons on startup
+            for mid in $(aerospace list-monitors | cut -c1); do
+              for sid in $(aerospace list-workspaces --monitor $mid --empty no); do
+              apps=$(aerospace list-windows --workspace "$sid" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
+
+              sketchybar --set space.$sid drawing=on
+
+              icon_strip=" "
+              if [ "''\${apps}" != "" ]; then
+                while read -r app; do
+                  icon_strip+=" $($CONFIG_DIR/plugins/icon_map_fn.sh "$app")"
+                done <<<"''\${apps}"
+              else
+                icon_strip=""
+              fi
+              sketchybar --set space.$sid label="$icon_strip"
+              done
+            done
+
+            sketchybar --add item space_separator left \
+              --set space_separator icon="💩" \
+              icon.padding_left=4 \
+              label.drawing=off \
+              background.drawing=off \
+              script="$PLUGIN_DIR/space_windows.sh" \
+              --subscribe space_separator aerospace_workspace_change front_app_switched space_windows_change aerospace_monitor_change
+
+            # Front app!!
+            sketchybar --add item front_app left \
+              --set front_app icon.drawing=off \
+              script="$PLUGIN_DIR/front_app.sh" \
+              --subscribe front_app front_app_switched
+
+            ##### Adding Right Items #####
+            # In the same way as the left items we can add items to the right side.
+            # Additional position (e.g. center) are available, see:
+            # https://felixkratz.github.io/SketchyBar/config/items#adding-items-to-sketchybar
+
+            # Some items refresh on a fixed cycle, e.g. the clock runs its script once
+            # every 10s. Other gititems respond to events they subscribe to, e.g. the
+            # volume.sh script is only executed once an actual change in system audio
+            # volume is registered. More info about the event system can be found here:
+            # https://felixkratz.github.io/SketchyBar/config/events
+
+            sketchybar --add item clock right \
+              --set clock update_freq=10 script="$PLUGIN_DIR/clock.sh" \
+              --add item volume right \
+              --set volume script="$PLUGIN_DIR/volume.sh" \
+              --subscribe volume volume_change \
+              --add item battery right \
+              --set battery update_freq=120 script="$PLUGIN_DIR/battery.sh" \
+              --subscribe battery system_woke power_source_change \
+              --add item swap e \
+              --set swap update_freq=20 script="$PLUGIN_DIR/memswap.sh" \
+              icon="" \
+              label.font="Hack Nerd Font:Italic:13.0" \
+              icon.color=0x44FFFFFF \
+              label.color=0x44FFFFFF
+
+            ##### Force all scripts to run the first time (never do this in a script) #####
             sketchybar --update
-            echo "sketchybar configuration loaded.."
           '';
+          services.sketchybar.extraPackages = [ pkgs.jq ];
 
           # Enable alternative shell support in nix-darwin.
           # programs.fish.enable = true;
