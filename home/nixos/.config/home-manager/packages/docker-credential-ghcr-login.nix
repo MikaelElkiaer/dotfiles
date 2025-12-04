4f23fa1e5282ed6ac5d26feb16ee5138fd686c3f
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2025-12-04 10:40:26 +0100

    chore(hm): Update flake
    docker-credential-ghcr-login-unstable: 2025-05-30 → 2025-12-04, [32;1m-82.7 KiB[0m
    home-manager: 0-unstable-2025-11-27 → 0-unstable-2025-11-30
    hyperfine: 1.19.0 → 1.20.0, [32;1m-15.0 KiB[0m
    lowdown: 2.0.2 → 2.0.4, [31;1m+17.1 KiB[0m

--- home/nixos/.config/home-manager/packages/docker-credential-ghcr-login.nix
+++ home/nixos/.config/home-manager/packages/docker-credential-ghcr-login.nix
@@ -5,16 +5,16 @@
 
 buildGoModule rec {
   pname = "docker-credential-ghcr-login";
-  version = "unstable-2025-05-30";
+  version = "unstable-2025-12-04";
 
   src = fetchFromGitHub {
     owner = "mikaelelkiaer";
     repo = "docker-credential-ghcr-login";
-    rev = "14bd0975d55d9aa43b24f66a85f747d0466828c2"; # main
-    sha256 = "1cc5di3msgwjdh3rin3kfcl8w08282rssanf5m90r27x30cn66rd";
+    rev = "f850028b4ed00a2964c86896042dc0f06e4797f2"; # main
+    sha256 = "1q6mg62h8ayasj0dy2a1x4psnmbs67wkhnbmzqf9qpaykw08yrrm";
   };
 
-  vendorHash = "sha256-z60OgpWuVcZplSvOAwxqB/S6DT6kh3mNUdYrLHArpOg=";
+  vendorHash = "sha256-+sedmiB1v5WMc+hOZyWBBU5nN7mlP2HTJL2qWqeb/jM=";
 
   ldflags = [
     "-s"
