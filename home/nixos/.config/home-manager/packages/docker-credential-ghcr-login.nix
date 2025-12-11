004787d1d3a09b9cabfd6db902075901ee079153
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2025-12-11 15:13:05 +0100

    chore(hm): Update flake
    docker-credential-ghcr-login-unstable: 2025-12-04 → 2025-12-11
    fluxcd: 2.7.4 → 2.7.5
    github-copilot-cli: 0.0.365 → 0.0.367, [31;1m+1239.8 KiB[0m
    go-containerregistry: 0.20.6 → 0.20.7, [31;1m+49.0 KiB[0m
    helm-ls: 0.4.1 → 0.5.4, [31;1m+398.3 KiB[0m
    home-manager: 0-unstable-2025-11-30 → 0-unstable-2025-12-05, [31;1m+30.2 KiB[0m
    index-aarch64: [31;1m+377.5 KiB[0m
    lazygit: 0.56.0 → 0.57.0, [32;1m-128.3 KiB[0m
    libgit2: 1.9.1 → 1.9.2
    nix: 2.31.2 → 2.31.2+1
    nix-cmd: 2.31.2 → 2.31.2+1
    nix-expr: 2.31.2 → 2.31.2+1
    nix-fetchers: 2.31.2 → 2.31.2+1
    nix-flake: 2.31.2 → 2.31.2+1
    nix-main: 2.31.2 → 2.31.2+1
    nix-store: 2.31.2 → 2.31.2+1
    nix-util: 2.31.2 → 2.31.2+1
    python3.13-python-lsp-server: 1.13.2 → 1.14.0
    skopeo: 1.20.0 → 1.21.0, [32;1m-635.0 KiB[0m
    terraform: 1.14.0 → 1.14.1, [31;1m+16.8 KiB[0m
    tmux: 3.6 → 3.6a

--- home/nixos/.config/home-manager/packages/docker-credential-ghcr-login.nix
+++ home/nixos/.config/home-manager/packages/docker-credential-ghcr-login.nix
@@ -5,16 +5,16 @@
 
 buildGoModule rec {
   pname = "docker-credential-ghcr-login";
-  version = "unstable-2025-12-04";
+  version = "unstable-2025-12-11";
 
   src = fetchFromGitHub {
     owner = "mikaelelkiaer";
     repo = "docker-credential-ghcr-login";
-    rev = "f850028b4ed00a2964c86896042dc0f06e4797f2"; # main
-    sha256 = "1q6mg62h8ayasj0dy2a1x4psnmbs67wkhnbmzqf9qpaykw08yrrm";
+    rev = "ed98a2dbfeb916380c06e112c096940aab3d18d0"; # main
+    sha256 = "1nzc212rjr5k1dkaz7js24d4n8hr7scp3rfh5509m2nncy98ma71";
   };
 
-  vendorHash = "sha256-+sedmiB1v5WMc+hOZyWBBU5nN7mlP2HTJL2qWqeb/jM=";
+  vendorHash = "sha256-fsQbn3/JmVeRFD+Pxm+FV7t1fY4zak3BLXtAM1EcLMk=";
 
   ldflags = [
     "-s"
