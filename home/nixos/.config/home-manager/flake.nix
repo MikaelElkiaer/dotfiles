8ca6264ff16cafc72c26ff3a8a1f7058f96b4361
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2026-01-30 15:19:52 +0100

    hm: Pin swift to master

--- home/nixos/.config/home-manager/flake.nix
+++ home/nixos/.config/home-manager/flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs_master.url = "github:nixos/nixpkgs/master";
     nixpkgs_stable.url = "github:nixos/nixpkgs/release-25.05";
     # INFO: Pre 0.17 - https://github.com/NixOS/nixpkgs/commit/af57e99c785f334638402ffd7b5e4565a0379461
     nixpkgs_gemini.url = "github:nixos/nixpkgs/af57e99c785f334638402ffd7b5e4565a0379461";
@@ -24,6 +25,7 @@
   outputs =
     inputs@{
       nixpkgs,
+      nixpkgs_master,
       nixpkgs_stable,
       nixpkgs_gemini,
       nix-index-database,
@@ -42,6 +44,7 @@
         # INFO: https://github.com/NixOS/nixpkgs/pull/450333
         awscli2 = (import nixpkgs_stable { system = prev.system; }).awscli2;
         gemini-cli = (import nixpkgs_gemini { system = prev.system; }).gemini-cli;
+        swift = (import nixpkgs_master { system = prev.system; }).swift;
       };
     in
     {
